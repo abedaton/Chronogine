5d9103043277b5761beda74c565edd5e70150620
Antoine Bedaton <antoine.bedaton@hotmail.fr>
2022-06-10 00:08:48 +0200

    Revert "Setup done"
    This reverts commit 77613e368512cc7d3bab2bd1e9bd4e8097e703a5.

--- Chronogine/src/Chronogine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Chronogine Applications
-#include <stdio.h>
-#include "Chronogine\Application.h"
-
-// ---Entry Point----
-#include "Chronogine\EntryPoint.h"
-//-------------------
